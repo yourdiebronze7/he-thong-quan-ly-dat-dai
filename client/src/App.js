14af7dba2a88e4f5b17e9e8d43bc09dafca7663e
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-05 11:18:26 -0700

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hệ Thống Quản Lý Đất Đai</h1>
+      <p>Chào mừng bạn đến với hệ thống quản lý đất đai sử dụng blockchain!</p>
+    </div>
+  );
+}
+
+export default App;
